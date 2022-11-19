96ee62467d92b20f63796df6ddfb85d733b510ae
Yerco <yerco@hotmail.com>
2022-11-19 16:21:38 -0300

    Lazy loading design pattern

--- factory_customer.py
+++ factory_customer.py
@@ -6,10 +6,11 @@ class FactoryCustomer:
 
     _customers: dict = dict()
 
-    def __init__(self):
-        self._customers["Customer"] = Customer()
-        self._customers["Lead"] = Lead()
-
     def create(self, customer_type: str) -> CustomerBase:
+        # Lazy loading design pattern
+        if not self._customers:
+            self._customers["Customer"] = Customer()
+            self._customers["Lead"] = Lead()
+
         # RIP design pattern
         return self._customers[customer_type]
