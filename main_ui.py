4f54dc4d4a2e97b2278e07972ecfcd445f1fc8c9
Yerco <yerco@hotmail.com>
2022-11-19 11:26:38 -0300

    Refactoring middle layer (inheritance), adding the UI to comsume it.

---
+++ main_ui.py
@@ -0,0 +1,107 @@
+import sys
+import datetime
+
+from PyQt6.QtWidgets import (
+    QApplication,
+    QLabel,
+    QWidget,
+    QGridLayout,
+    QComboBox,
+    QMainWindow,
+    QFormLayout,
+    QLineEdit,
+    QTextEdit,
+    QPushButton,
+    QTableWidget,
+    QMessageBox,
+    QTableWidgetItem
+)
+
+
+# mixed naming conventions due to Qt's camel case
+class Window(QMainWindow):
+    def __init__(self):
+        super().__init__(parent=None)
+        self.setWindowTitle("PyQt Demo")
+        #self.setFixedSize(WINDOW_SIZE, WINDOW_SIZE)
+        self.generalLayout = QGridLayout()
+        # central widget is needed
+        centralWidget = QWidget(self)
+        centralWidget.setLayout(self.generalLayout)
+        self.setCentralWidget(centralWidget)
+        self._createSelectCustomerCombo()
+        self._createCustomerNameEntry()
+        self._createPhoneNumberEntry()
+        self._createBillAmountEntry()
+        self._createBillDateEntry()
+        self._createAddressEntry()
+        self._createValidateButton()
+        self._createDataTable()
+
+    def _createSelectCustomerCombo(self):
+        self.customer_combo_box = QComboBox()
+        self.customer_combo_box.addItems(["", "Customer", "Lead"])
+        layout = QFormLayout()
+        layout.addRow(QLabel("Customer Type"), self.customer_combo_box)
+        self.generalLayout.addLayout(layout, 0, 0)
+        self.customer_combo_box.activated.connect(self._combobox_select)
+
+    def _combobox_select(self, txt_selection):
+        self.cust_type = self.customer_combo_box.currentText()
+
+    def _createCustomerNameEntry(self):
+        layout = QFormLayout()
+        self.customer_name = QLineEdit()
+        layout.addRow(QLabel("Customer Name"), self.customer_name)
+        self.generalLayout.addLayout(layout, 1, 0)
+
+    def _createPhoneNumberEntry(self):
+        layout = QFormLayout()
+        self.phone_number = QLineEdit()
+        layout.addRow(QLabel("Phone Number"), self.phone_number)
+        self.generalLayout.addLayout(layout, 2, 0)
+
+    def _createBillAmountEntry(self):
+        layout = QFormLayout()
+        self.bill_amount = QLineEdit()
+        layout.addRow(QLabel("Bill Amount"), self.bill_amount)
+        self.generalLayout.addLayout(layout, 0, 1)
+
+    def _createBillDateEntry(self):
+        layout = QFormLayout()
+        self.bill_date = QLineEdit()
+        layout.addRow(QLabel("Bill Date"), self.bill_date)
+        self.generalLayout.addLayout(layout, 1, 1)
+
+    def _createAddressEntry(self):
+        layout = QFormLayout()
+        self.address = QTextEdit()
+        self.address.setFixedSize(250, 70)
+        layout.addRow(QLabel("Address"), self.address)
+        self.generalLayout.addLayout(layout, 2, 1)
+
+    def _createDataTable(self):
+        table = QTableWidget()
+        table.setRowCount(3)
+        table.setColumnCount(7)
+        self.generalLayout.addWidget(table, 4, 0, 1, 2)
+
+    def _createValidateButton(self):
+        button = QPushButton("Validate")
+        self.generalLayout.addWidget(button, 3, 0)
+        button.clicked.connect(self._customer_validation)
+
+    def _customer_validation(self):
+        ...
+
+
+def main():
+    app = QApplication([])
+    window = Window()
+    window.setWindowTitle("PyQt Demo")
+    window.show()
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()
--- middle_layer.py
+++ middle_layer.py
@@ -1,4 +1,9 @@
-class Customer:
+import datetime
+
+from abc import ABC, abstractmethod
+
+
+class CustomerBase(ABC):
     def __init__(self, customer_name: str = None, phone_number: str = None, bill_amount: str = None,
                  bill_date: str = None, address: str = None):
         self.customer_name: str = customer_name
@@ -7,13 +12,36 @@ class Customer:
         self.bill_date: str = bill_date
         self.address: str = address
 
+    @abstractmethod
+    def validate(self) -> None:
+        raise NotImplementedError
 
-# Lead is a Customer with fewer validations
-class Lead:
-    def __init__(self, customer_name: str = None, phone_number: str = None, bill_amount: str = None,
-                 bill_date: str = None, address: str = None):
-        self.customer_name: str = customer_name
-        self.phone_number: str = phone_number
-        self.bill_amount: str = bill_amount
-        self.bill_date: str = bill_date
-        self.address: str = address
+
+class Customer(CustomerBase):
+
+    def __init__(self, *args, **kwargs):
+        super(CustomerBase, self).__init__(*args, **kwargs)
+
+    def validate(self) -> None:
+        if len(self.customer_name) == 0:
+            raise Exception("Customer name is required")
+        if len(self.phone_number) == 0:
+            raise Exception("Phone number is required")
+        if len(self.bill_amount) == 0:
+            raise Exception("Bill amount is required")
+        if datetime.datetime.strptime(self.bill_date, '%d-%m-%Y') > datetime.datetime.now():
+            raise Exception("Bill date is not proper")
+        if len(self.address) == 0:
+            raise Exception("Address is required")
+
+
+class Lead(CustomerBase):
+
+    def __init__(self, *args, **kwargs):
+        super(CustomerBase, self).__init__(*args, **kwargs)
+
+    def validate(self) -> None:
+        if len(self.customer_name) == 0:
+            raise Exception("Customer name is required")
+        if len(self.phone_number) == 0:
+            raise Exception("Phone number is required")
